64610e4de2fd05fa8aac424e0539f9dc8daf3941
Abdelrahman Ashraf <abdelrahmanashraf6000@gmail.com>
2025-07-17 16:36:15 +0300

    feat : added find_student_by_id Function
    added added find_student_by_id successfully

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import json
 DB_FILE = "students.json"
+
+def find_student_by_id(students, student_id):
+    for student in students:
+        if student["id"] == student_id:
+            return student
+    return None
