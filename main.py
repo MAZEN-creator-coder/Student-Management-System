d7b293df9e00f07a7c5caba0769fb2d0f9305433
Mohammed Yasser <memomohammed.2005@gmail.com>
2025-07-17 16:27:32 +0300

    feat : added view_all_student Function
    done

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import json
 DB_FILE = "students.json"
+def view_all_students(students):
+    if not students:
+        print("No students found.")
+        return
+    for s in students:
+        print(f"ID: {s['id']}, Name: {s['name']}, Grade: {s['grade']}")
